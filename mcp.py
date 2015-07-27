e7d8637be1098b63cf14d4d945f83e3df98df59d
Mike Smith <neodd70@gmail.com>
2015-07-27 19:04:00 -0400

    Latest changes
    Saved latest changes. Changes included time delay and button state.

--- mcp.py
+++ mcp.py
@@ -5,20 +5,24 @@ sys.path.append('.')
 import lights.occupancy
 import daemon
 
-MASTER_BATH = lights.occupancy.Occupancy("192.168.1.105", 10)
+MASTER_BATH = lights.occupancy.Occupancy("192.168.1.130", 180)
 
 @app.route('/master_bath')
 def master_bath():
-	pir_state = int(request.args.get("pir_state", 0))
-	MASTER_BATH.trigger(pir_state)
-	return "Master Bath Method worked"
-
-
-@app.route('/lights_mb')
-def lights_mb():
-	pir_state=request.args.get("pir_state", 0)
-	MASTER_BATH.trigger(pir_state)
-	return "lights_mb"
+	try:
+		pir_state = int(request.args.get("pir_state", 0))
+		button_pressed = int(request.args.get("button_pressed", 0))
+		print "pir_state:", pir_state
+		print "button_pressed:", button_pressed
+		print "_button_override:", MASTER_BATH._button_override
+		if pir_state: 
+			MASTER_BATH.trigger(pir_state)
+		if button_pressed:
+			MASTER_BATH.button(button_pressed)	
+	except Exception, error:
+		print error
+		return error
+	return "What has happened - PIR is {0} Button Pressed is {1} Button Override is {2}".format(pir_state, button_pressed, MASTER_BATH._button_override)
 
 if __name__ == '__main__':
 	with daemon.DaemonContext():
--- occupancy.py
+++ occupancy.py
@@ -8,11 +8,13 @@ import threading
 
 class Occupancy(object):
 	"""This is to turn the Hue light on in the master bath when the PIR sensor detects motion"""
-	def __init__(self,bridge_ip,duration=10):
+	BULB_NUMBER=3
+	def __init__(self,bridge_ip,duration=180):
 		self._Timer_thread = None
 		self.bridge_ip = bridge_ip
 		self._bridge_connect()
 		self._duration = duration
+		self._button_override = False
 		
 	def _bridge_connect(self):
 		self._b = Bridge(self.bridge_ip) # Enter bridge IP here.
@@ -23,61 +25,89 @@ class Occupancy(object):
 	def _hue_for_given_time(self):
 		"""This sets the color hue of the light for the time of day"""
 		hour = datetime.datetime.now().hour
-		if hour >= 23 and hour <= 7:
-			return 47125 #hue blue
+		if hour >= 23 or hour <= 7:
+			return 47125 #hue blue			
 		elif hour >= 8 and hour <= 22:
-			return 14910 #hue red
+			return 14910 #hue white
+			
 
 	def _sat_for_given_time(self):
 		"""This sets the saturation of the light for the time of day"""
 		hour = datetime.datetime.now().hour
-		if hour >= 23 and hour <= 7:
-			return 253 #sat 
+		if hour >= 23 or hour <= 7:
+			return 253 #sat			
 		elif hour >= 8 and hour <= 22:
 			return 144 #sat
+			
 
 	def _bri_for_given_time(self):
 		"""This sets the brightness of the light for the time of day"""
 		hour = datetime.datetime.now().hour
-		if hour >= 23 and hour <= 7:
-			return 7 #bri 5 percent
+		if hour >= 23 or hour <=7:
+			return 7 #bri 5 percent			
 		elif hour >= 8 and hour <= 22:
 			return 254 #bri 100 percent
+			
 
 
 	def trigger(self, state):
 		"""Handles the trigger event of the PIR sensor"""
 		if state == 1:
+			if self._button_override == True:
+				pass
+			else:
+				self.reset_counter()
+				self.motion_on()
+
+
+	def button(self, state):
+		"""Handles the button press event"""
+		if state == 1:
+			self._button_override = True
+			self.reset_counter(600)#seconds
 			self.motion_on()
-			self.reset_counter()
 
 	
-	def reset_counter(self):
+	def reset_counter(self, duration=None):
 		"""Resets timer if motion is detected"""
+		if duration is None:
+			duration = self._duration
 		if self._Timer_thread is not None:
 			self._Timer_thread.cancel()
 			self._Timer_thread=None
 		
-		self._Timer_thread = threading.Timer(self._duration, self.motion_off)
+		self._Timer_thread = threading.Timer(duration, self.motion_off)
 		self._Timer_thread.start()
 
 		
-		
-
 	def motion_on(self):
-		
+		"""Sets light brightness and color according to button press or PIR state"""
 		lights = self._b.get_light_objects()
+		if self._button_override == True:
+			self._b.set_light(self.BULB_NUMBER, 'on', True, transitiontime=20)
+			time.sleep(.02)
+			self._b.set_light(self.BULB_NUMBER, 'bri', 254 , transitiontime=20)
+			time.sleep(.02)
+			self._b.set_light(self.BULB_NUMBER, 'hue', 14910 , transitiontime=20)
+			time.sleep(.02)
+			self._b.set_light(self.BULB_NUMBER, 'sat', 144 , transitiontime=20)
+			time.sleep(.02)
 
-		self._b.set_light(5, 'on', True, transitiontime=0)
-		self._b.set_light(5, 'bri', self._bri_for_given_time(), transitiontime=0)
-		self._b.set_light(5, 'hue', self._hue_for_given_time(), transitiontime=0)
-		self._b.set_light(5, 'sat', self._sat_for_given_time(), transitiontime=0)
+		else:
+			self._b.set_light(self.BULB_NUMBER, 'on', True, transitiontime=20)
+			time.sleep(.02)
+			self._b.set_light(self.BULB_NUMBER, 'bri', self._bri_for_given_time(), transitiontime=20)
+			time.sleep(.02)
+			self._b.set_light(self.BULB_NUMBER, 'hue', self._hue_for_given_time(), transitiontime=20)
+			time.sleep(.02)
+			self._b.set_light(self.BULB_NUMBER, 'sat', self._sat_for_given_time(), transitiontime=20)
+			time.sleep(.02)
 
 	def motion_off(self):
 
 		lights = self._b.get_light_objects()
-
-		self._b.set_light(5, 'on', False, transitiontime=0)
+		self._button_override = False
+		self._b.set_light(self.BULB_NUMBER, 'on', False, transitiontime=30)
 		
 		
 if __name__ =="__main__":
